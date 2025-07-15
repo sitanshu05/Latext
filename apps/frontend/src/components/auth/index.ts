10d6a22365513efbc844d78eb4710d06c558689d
--add <sitanshuhallad2002@gmail.com>
2025-07-16 02:56:33 +0530

    feat: add auth components index for easier imports

---
+++ apps/frontend/src/components/auth/index.ts
@@ -0,0 +1,5 @@
+// Authentication Components
+export { default as LoginForm } from './LoginForm'
+export { default as SignupForm } from './SignupForm'
+export { default as GoogleAuthButton } from './GoogleAuthButton'
+export { default as LogoutButton } from './LogoutButton' 
